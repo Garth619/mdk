1589bd43b06c53c20c1de3cc63a36c3dbe1728a2
Garrett Cullen <Garth619@users.noreply.github.com>
2021-08-23 16:21:48 -0700

    push

--- wp-content/themes/mdk/page-templates/includes/homepage_template_parts/section-1.php
+++ wp-content/themes/mdk/page-templates/includes/homepage_template_parts/section-1.php
@@ -2,6 +2,40 @@
 
   <div id='sec-one-inner'>
 
+    <div id='sec-one-left'>
+
+      <span id='sec-one-subtitle'>Experienced Personal injury attorneys</span><!-- sec-one-subtitle -->
+
+      <div id='sec-one-image'>
+
+        <picture>
+
+          <!-- <source media='(min-width: 1380px)' data-srcset='<?php // bloginfo('template_directory'); ?>/images'> -->
+
+          <img src='<?php bloginfo('template_directory');?>/images/hero-att-img-mobile.jpg' alt='' />
+
+        </picture>
+
+      </div><!-- sec-one-image -->
+
+      <span id='sec-one-title'>Dedicated to the best results for our clients, every time.</span><!-- sec-one-title -->
+
+      <a class='button' href='#consultation'>Request free consultation</a><!-- button -->
+
+    </div><!-- sec-one-left -->
+
+    <div id='sec-one-right'>
+
+    </div><!-- sec-one-right -->
+
   </div><!-- sec-one-inner -->
+
+  <picture>
+
+    <!-- <source media='(min-width: 1380px)' data-srcset='<?php // bloginfo('template_directory'); ?>/images'> -->
+
+    <img id='hero-image' src='<?php bloginfo('template_directory');?>/images/hero-bg-img-768.jpg' alt='' />
+
+  </picture>
 
 </section><!-- section-one -->
