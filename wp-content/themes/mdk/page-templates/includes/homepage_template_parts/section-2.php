17ce8a680e4da8c27dc63accdec7a0fe0508485a
Garrett Cullen <Garth619@users.noreply.github.com>
2021-08-25 16:59:55 -0700

    Added my project

--- wp-content/themes/mdk/page-templates/includes/homepage_template_parts/section-2.php
+++ wp-content/themes/mdk/page-templates/includes/homepage_template_parts/section-2.php
@@ -1,7 +1,99 @@
 <section id='section-two'>
 
-  <div id='sec-two-inner'>
+  <div id='sec-two-container' class='content'>
 
-  </div><!-- sec-two-inner -->
+    <h1>Surprise Personal
+      Injury Lawyers</h1>
+
+    <div id='sec-two-inner'>
+
+      <div id='sec-two-top'>
+
+        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h2>
+
+        <p>Morbi viverra lacus ut erat hendrerit feugiat. Cras cursus tempus volutpat. Maecenas sit amet sem sit amet
+          ligula pulvinar varius. Nunc vehicula tempus erat vel hendrerit. Phasellus pulvinar euismod cursus. Duis sit
+          amet libero eget tellus venenatis scelerisque. Sed nec nunc id ligula vestibulum lobortis. Nunc feugiat mattis
+          lectus vitae porta. Maecenas vulputate eros id maximus vulputate. Ut vitae augue semper, sodales est id,
+          auctor
+          leo. Praesent sit amet tincidunt lacus. Etiam lobortis diam et viverra consequat. Duis vulputate sem in augue
+          vestibulum maximus. In hac habitasse platea dictumst.</p>
+
+        <p>Etiam metus lectus, convallis id diam ac, sodales sodales nulla. Nulla ac nisl eros. Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit. Donec mi tortor, auctor eget nulla et, pulvinar aliquam ligula. Maecenas
+          sed
+          posuere sapien. Nunc eu quam et metus facilisis feugiat sed in elit. Pellente sque scelerisque dui at dui
+          tristique tristique. Maecenas eleifend eu magna vitae ultrices. Cras sagittis augue eros, non tempus est
+          dapibus
+          eget. Etiam convallis bibendum libero, id rutrum mauris commodo sit amet. Duis varius egestas quam ut
+          eleifend.
+          Aliquam laoreet nec lectus ut lacinia.</p>
+
+        <h3>Why choose our law firm?</h3>
+
+        <ul>
+          <li>Suspendisse potenti. In elementum ante a quam consectetur finibus onec luctus, purus vitae fringilla
+            laoreet, neque massa feugiat diam, non facilisis massa justo sed quam.</li>
+          <li>Proin risus dui, tristique ac sagittis eget, euismod at justo.</li>
+          <li>Donec dignissim, risus a fringilla dapibus, dolor dui mattis est, et semper mauris sem at lacus donec
+            pretium nunc vel dolor congue, ac ultricies mi scelerisque.</li>
+          <li>Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</li>
+          <li>Quisque posuere vitae purus non vestibulum. Ut odio neque, rutrum non ullamcorper ac, mattis sed ante.
+          </li>
+        </ul>
+
+      </div><!-- sec-two-top -->
+
+      <div id='sec-two-middle'>
+
+        <div id='sec-two-image-wrapper'>
+
+          <div id='sec-two-image-inner'>
+
+            <picture>
+
+              <source media='(min-width: 1380px)'
+                srcset='<?php bloginfo('template_directory');?>/images/content-image.jpg'>
+
+              <img id='sec-two-image' src='<?php bloginfo('template_directory');?>/images/content-image-mobile.jpg'
+                alt='' />
+
+            </picture>
+
+          </div><!-- sec-two-image-inner -->
+
+        </div><!-- sec-two-image-wrapper -->
+
+        <div id='sec-two-middle-block'>
+
+          <p>Our clients are treated with the <strong>greatest care</strong> and have <strong>qualified
+              attorneys</strong>
+            to work with
+            throughout the
+            duration of their case, that in turn <strong>provides confidence</strong> in the representation. </p>
+
+        </div><!-- sec-two-middle-block -->
+
+      </div><!-- sec-two-middle -->
+
+      <div id='sec-two-bottom'>
+
+        <p>Morbi viverra lacus ut erat hendrerit feugiat. Cras cursus tempus volutpat. Maecenas sit amet sem sit amet
+          ligula pulvinar varius. Nunc vehicula tempus erat vel hendrerit. Phasellus pulvinar euismod cursus. Duis sit
+          amet libero eget tellus venenatis scelerisque. Sed nec nunc id ligula vestibulum lobortis. Nunc feugiat mattis
+          lectus vitae porta. Maecenas vulputate eros id maximus vulputate. Ut vitae augue semper, sodales est id,
+          auctor
+          leo. Praesent sit amet tincidunt lacus. Etiam lobortis diam et viverra consequat. Duis vulputate sem in augue
+          vestibulum maximus. In hac habitasse platea dictumst. Etiam metus lectus, convallis id diam ac, sodales
+          sodales
+          nulla. Nulla ac nisl eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mi tortor, auctor
+          eget
+          nulla et, pulvinar aliquam ligula. Maecenas sed posuere sapien.</p>
+
+      </div><!-- sec-two-bottom -->
+
+    </div><!-- sec-two-inner -->
+
+  </div><!-- sec-two-container -->
 
 </section><!-- section-two -->
