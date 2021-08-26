2787f760c05fcfad41a2132fc6114b8b7ea642ea
Garrett Cullen <Garth619@users.noreply.github.com>
2021-08-25 18:07:13 -0700

    Added my project

--- wp-content/themes/mdk/page-templates/includes/homepage_template_parts/section-3.php
+++ wp-content/themes/mdk/page-templates/includes/homepage_template_parts/section-3.php
@@ -1,3 +1,14 @@
 <section id='section-three'>
 
+  <div id='sec-three-content'>
+
+    <span id='sec-three-title'>LATEST CASE RESULTs</span><!-- sec-three-title -->
+
+    <span id='sec-three-descrip'>We advocate for your best result, that’s our promise.</span><!-- sec-three-descrip -->
+
+    <a id='sec-three-button' class='button-two' href='<?php bloginfo('url');?>/href'>View All Results</a>
+    <!-- sec-three-button -->
+
+  </div><!-- sec-three-content -->
+
 </section><!-- section-three -->
