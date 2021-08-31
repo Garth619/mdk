3348b136802456f70c2fd038d18d8cff94a5d41c
Garrett Cullen <Garth619@users.noreply.github.com>
2021-08-31 14:38:09 -0700

    Added my project

--- wp-content/themes/mdk/page-templates/includes/homepage_template_parts/section-5.php
+++ wp-content/themes/mdk/page-templates/includes/homepage_template_parts/section-5.php
@@ -2,136 +2,200 @@
 
   <div id='sec-five-inner'>
 
-    <div id='sec-five-slider'>
+    <div id='sec-five-slider-wrapper'>
 
-      <div class='sec-five-slide'>
+      <div id='sec-five-slider'>
 
-        <img class='sec-five-slide-quote' src='<?php bloginfo('template_directory');?>/images/quote.svg' alt='' />
+        <div class='sec-five-slide'>
 
-        <span class='sec-five-slide-title'>Sargon was outstanding! Very professional and super responsive.</span>
-        <!-- sec-five-slide-title -->
+          <img class='sec-five-slide-quote' src='<?php bloginfo('template_directory');?>/images/quote.svg' alt='' />
 
-        <span class='sec-five-slide-descrip'>Always took the time to go over every detail of the case thoroughly and
-          guided me all along. Got me exactly what I deserved! Very pleased with his work. I highly recommend Sargon at
-          MDK if you have a personal injury case. As good as it gets. Thank you!</span>
-        <!-- sec-five-slide-descrip -->
+          <div class='sec-five-slide-inner'>
 
-        <span class='sec-five-slide-name'>Andres Alonzo</span>
-        <!-- sec-five-slide-name -->
+            <span class='sec-five-slide-title'>Sargon was outstanding! Very professional and super responsive.</span>
+            <!-- sec-five-slide-title -->
 
-        <div class='sec-five-slide-icon-wrapper'>
+            <span class='sec-five-slide-descrip'>Always took the time to go over every detail of the case thoroughly and
+              guided me all along. Got me exactly what I deserved! Very pleased with his work. I highly recommend Sargon
+              at
+              MDK if you have a personal injury case. As good as it gets. Thank you!</span>
+            <!-- sec-five-slide-descrip -->
 
-          <img id='sec-five-slide-icon' src='<?php bloginfo('template_directory');?>/images/google-logo.png' alt='' />
+            <span class='sec-five-slide-name'>Andres Alonzo</span>
+            <!-- sec-five-slide-name -->
 
-          <span class='sec-five-icon-title'>Review</span><!-- sec-five-icon-title -->
+            <div class='sec-five-slide-icon-wrapper'>
 
-        </div><!-- sec-five-slide-icon-wrapper -->
+              <img id='sec-five-slide-icon' src='<?php bloginfo('template_directory');?>/images/google-logo.png'
+                alt='' />
 
-      </div><!-- sec-five-slide -->
+              <span class='sec-five-icon-title'>Review</span><!-- sec-five-icon-title -->
 
-      <div class='sec-five-slide'>
+            </div><!-- sec-five-slide-icon-wrapper -->
 
-        <img class='sec-five-slide-quote' src='<?php bloginfo('template_directory');?>/images/quote.svg' alt='' />
+          </div><!-- sec-five-slide-inner -->
 
-        <span class='sec-five-slide-title'>Sargon was outstanding! Very professional and super responsive.</span>
-        <!-- sec-five-slide-title -->
+        </div><!-- sec-five-slide -->
 
-        <span class='sec-five-slide-descrip'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-          esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-          officia deserunt mollit anim id est laborum.</span>
-        <!-- sec-five-slide-descrip -->
+        <div class='sec-five-slide'>
 
-        <span class='sec-five-slide-name'>Andres Alonzo</span>
-        <!-- sec-five-slide-name -->
+          <img class='sec-five-slide-quote' src='<?php bloginfo('template_directory');?>/images/quote.svg' alt='' />
 
-        <div class='sec-five-slide-icon-wrapper'>
+          <div class='sec-five-slide-inner'>
 
-          <img id='sec-five-slide-icon' src='<?php bloginfo('template_directory');?>/images/google-logo.png' alt='' />
+            <span class='sec-five-slide-title'>Sargon was outstanding! Very professional and super responsive.</span>
+            <!-- sec-five-slide-title -->
 
-          <span class='sec-five-icon-title'>Review</span><!-- sec-five-icon-title -->
+            <span class='sec-five-slide-descrip'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+            <!-- sec-five-slide-descrip -->
 
-        </div><!-- sec-five-slide-icon-wrapper -->
+            <span class='sec-five-slide-name'>Andres Alonzo</span>
+            <!-- sec-five-slide-name -->
 
-      </div><!-- sec-five-slide -->
+            <div class='sec-five-slide-icon-wrapper'>
 
-      <div class='sec-five-slide'>
+              <img id='sec-five-slide-icon' src='<?php bloginfo('template_directory');?>/images/google-logo.png'
+                alt='' />
 
-        <img class='sec-five-slide-quote' src='<?php bloginfo('template_directory');?>/images/quote.svg' alt='' />
+              <span class='sec-five-icon-title'>Review</span><!-- sec-five-icon-title -->
 
-        <span class='sec-five-slide-title'>Sargon was outstanding! Very professional and super responsive.</span>
-        <!-- sec-five-slide-title -->
+            </div><!-- sec-five-slide-icon-wrapper -->
 
-        <span class='sec-five-slide-descrip'>Always took the time to go over every detail of the case thoroughly and
-          guided me all along. Got me exactly what I deserved! Very pleased with his work. I highly recommend Sargon at
-          MDK if you have a personal injury case. As good as it gets. Thank you!</span>
-        <!-- sec-five-slide-descrip -->
+          </div><!-- sec-five-slide-inner -->
 
-        <span class='sec-five-slide-name'>Andres Alonzo</span>
-        <!-- sec-five-slide-name -->
+        </div><!-- sec-five-slide -->
 
-        <div class='sec-five-slide-icon-wrapper'>
+        <div class='sec-five-slide'>
 
-          <img id='sec-five-slide-icon' src='<?php bloginfo('template_directory');?>/images/google-logo.png' alt='' />
+          <img class='sec-five-slide-quote' src='<?php bloginfo('template_directory');?>/images/quote.svg' alt='' />
 
-          <span class='sec-five-icon-title'>Review</span><!-- sec-five-icon-title -->
+          <div class='sec-five-slide-inner'>
 
-        </div><!-- sec-five-slide-icon-wrapper -->
+            <span class='sec-five-slide-title'>Sargon was outstanding! Very professional and super responsive.</span>
+            <!-- sec-five-slide-title -->
 
-      </div><!-- sec-five-slide -->
+            <span class='sec-five-slide-descrip'>Always took the time to go over every detail of the case thoroughly and
+              guided me all along. Got me exactly what I deserved! Very pleased with his work. I highly recommend Sargon
+              at
+              MDK if you have a personal injury case. As good as it gets. Thank you!</span>
+            <!-- sec-five-slide-descrip -->
 
-      <div class='sec-five-slide'>
+            <span class='sec-five-slide-name'>Andres Alonzo</span>
+            <!-- sec-five-slide-name -->
 
-        <img class='sec-five-slide-quote' src='<?php bloginfo('template_directory');?>/images/quote.svg' alt='' />
+            <div class='sec-five-slide-icon-wrapper'>
 
-        <span class='sec-five-slide-title'>Sargon was outstanding! Very professional and super responsive.</span>
-        <!-- sec-five-slide-title -->
+              <img id='sec-five-slide-icon' src='<?php bloginfo('template_directory');?>/images/google-logo.png'
+                alt='' />
 
-        <span class='sec-five-slide-descrip'>Always took the time to go over every detail of the case thoroughly and
-          guided me all along. Got me exactly what I deserved! Very pleased with his work. I highly recommend Sargon at
-          MDK if you have a personal injury case. As good as it gets. Thank you!</span>
-        <!-- sec-five-slide-descrip -->
+              <span class='sec-five-icon-title'>Review</span><!-- sec-five-icon-title -->
 
-        <span class='sec-five-slide-name'>Andres Alonzo</span>
-        <!-- sec-five-slide-name -->
+            </div><!-- sec-five-slide-icon-wrapper -->
 
-        <div class='sec-five-slide-icon-wrapper'>
+          </div><!-- sec-five-slide-inner -->
 
-          <img id='sec-five-slide-icon' src='<?php bloginfo('template_directory');?>/images/google-logo.png' alt='' />
+        </div><!-- sec-five-slide -->
 
-          <span class='sec-five-icon-title'>Review</span><!-- sec-five-icon-title -->
+        <div class='sec-five-slide'>
 
-        </div><!-- sec-five-slide-icon-wrapper -->
+          <img class='sec-five-slide-quote' src='<?php bloginfo('template_directory');?>/images/quote.svg' alt='' />
 
-      </div><!-- sec-five-slide -->
+          <div class='sec-five-slide-inner'>
 
-      <div class='sec-five-slide'>
+            <span class='sec-five-slide-title'>Sargon was outstanding! Very professional and super responsive.</span>
+            <!-- sec-five-slide-title -->
 
-        <img class='sec-five-slide-quote' src='<?php bloginfo('template_directory');?>/images/quote.svg' alt='' />
+            <span class='sec-five-slide-descrip'>Always took the time to go over every detail of the case thoroughly and
+              guided me all along. Got me exactly what I deserved! Very pleased with his work. I highly recommend Sargon
+              at
+              MDK if you have a personal injury case. As good as it gets. Thank you!</span>
+            <!-- sec-five-slide-descrip -->
 
-        <span class='sec-five-slide-title'>Sargon was outstanding! Very professional and super responsive.</span>
-        <!-- sec-five-slide-title -->
+            <span class='sec-five-slide-name'>Andres Alonzo</span>
+            <!-- sec-five-slide-name -->
 
-        <span class='sec-five-slide-descrip'>Always took the time to go over every detail of the case thoroughly and
-          guided me all along. Got me exactly what I deserved! Very pleased with his work. I highly recommend Sargon at
-          MDK if you have a personal injury case. As good as it gets. Thank you!</span>
-        <!-- sec-five-slide-descrip -->
+            <div class='sec-five-slide-icon-wrapper'>
 
-        <span class='sec-five-slide-name'>Andres Alonzo</span>
-        <!-- sec-five-slide-name -->
+              <img id='sec-five-slide-icon' src='<?php bloginfo('template_directory');?>/images/google-logo.png'
+                alt='' />
 
-        <div class='sec-five-slide-icon-wrapper'>
+              <span class='sec-five-icon-title'>Review</span><!-- sec-five-icon-title -->
 
-          <img id='sec-five-slide-icon' src='<?php bloginfo('template_directory');?>/images/google-logo.png' alt='' />
+            </div><!-- sec-five-slide-icon-wrapper -->
 
-          <span class='sec-five-icon-title'>Review</span><!-- sec-five-icon-title -->
+          </div><!-- sec-five-slide-inner -->
 
-        </div><!-- sec-five-slide-icon-wrapper -->
+        </div><!-- sec-five-slide -->
 
-      </div><!-- sec-five-slide -->
+        <div class='sec-five-slide'>
 
-    </div><!-- sec-five-slider -->
+          <img class='sec-five-slide-quote' src='<?php bloginfo('template_directory');?>/images/quote.svg' alt='' />
+
+          <div class='sec-five-slide-inner'>
+
+            <span class='sec-five-slide-title'>Sargon was outstanding! Very professional and super responsive.</span>
+            <!-- sec-five-slide-title -->
+
+            <span class='sec-five-slide-descrip'>Always took the time to go over every detail of the case thoroughly and
+              guided me all along. Got me exactly what I deserved! Very pleased with his work. I highly recommend Sargon
+              at
+              MDK if you have a personal injury case. As good as it gets. Thank you!</span>
+            <!-- sec-five-slide-descrip -->
+
+            <span class='sec-five-slide-name'>Andres Alonzo</span>
+            <!-- sec-five-slide-name -->
+
+            <div class='sec-five-slide-icon-wrapper'>
+
+              <img id='sec-five-slide-icon' src='<?php bloginfo('template_directory');?>/images/google-logo.png'
+                alt='' />
+
+              <span class='sec-five-icon-title'>Review</span><!-- sec-five-icon-title -->
+
+            </div><!-- sec-five-slide-icon-wrapper -->
+
+          </div><!-- sec-five-slide-inner -->
+
+        </div><!-- sec-five-slide -->
+
+        <div class='sec-five-slide'>
+
+          <img class='sec-five-slide-quote' src='<?php bloginfo('template_directory');?>/images/quote.svg' alt='' />
+
+          <div class='sec-five-slide-inner'>
+
+            <span class='sec-five-slide-title'>Sargon was outstanding! Very professional and super responsive.</span>
+            <!-- sec-five-slide-title -->
+
+            <span class='sec-five-slide-descrip'>Always took the time to go over every detail of the case thoroughly and
+              guided me all along. Got me exactly what I deserved! Very pleased with his work. I highly recommend Sargon
+              at
+              MDK if you have a personal injury case. As good as it gets. Thank you!</span>
+            <!-- sec-five-slide-descrip -->
+
+            <span class='sec-five-slide-name'>Andres Alonzo</span>
+            <!-- sec-five-slide-name -->
+
+            <div class='sec-five-slide-icon-wrapper'>
+
+              <img id='sec-five-slide-icon' src='<?php bloginfo('template_directory');?>/images/google-logo.png'
+                alt='' />
+
+              <span class='sec-five-icon-title'>Review</span><!-- sec-five-icon-title -->
+
+            </div><!-- sec-five-slide-icon-wrapper -->
+
+          </div><!-- sec-five-slide-inner -->
+
+        </div><!-- sec-five-slide -->
+
+      </div><!-- sec-five-slider -->
+
+    </div><!-- sec-five-slider-wrapper -->
 
     <div class='sec-five-image-wrapper'>
 
