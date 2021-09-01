1246c31e8a9e1b687f2f713fa28dab4f6552857e
Garrett Cullen <Garth619@users.noreply.github.com>
2021-08-31 19:32:21 -0700

    Added my project

--- wp-content/themes/mdk/page-templates/includes/homepage_template_parts/section-6.php
+++ wp-content/themes/mdk/page-templates/includes/homepage_template_parts/section-6.php
@@ -2,6 +2,31 @@
 
   <div id='sec-six-inner'>
 
+    <div id='sec-six-top'>
+
+      <span id='sec-six-subtitle'>BRING THE BEST TOGETHEr</span><!-- sec-six-subtitle -->
+
+      <span id='sec-six-title'>Passion.<br> Experience.<br> Diligence.</span><!-- sec-six-title -->
+
+    </div><!-- sec-six-top -->
+
+    <div id='sec-six-bottom' class='content'>
+
+      <p>Morbi viverra lacus ut erat hendrerit feugiat. Cras cursus tempus volutpat. Maecenas sit amet sem sit amet
+        ligula pulvinar varius. Nunc vehicula tempus erat vel hendrerit. Phasellus pulvinar euismod cursus. Duis sit
+        amet libero eget tellus venenatis scelerisque. Sed nec nunc id ligula vestibulum lobortis. Nunc feugiat mattis
+        lectus vitae porta. Maecenas vulputate eros id maximus vulputate. Ut vitae augue semper, sodales est id, auctor
+        leo. </p>
+
+      <p>Praesent sit amet tincidunt lacus. Etiam lobortis diam et viverra consequat. Duis vulputate sem in augue
+        vestibulum maximus. In hac habitasse platea dictumst.</p>
+
+    </div><!-- sec-six-bottom -->
+
   </div><!-- sec-six-inner -->
+
+  <div id='sec-six-awards-wrapper'>
+
+  </div><!-- sec-six-awards-wrapper -->
 
 </section><!-- section-six -->
