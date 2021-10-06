8e522a76aaf960fde9191edb6b93946127b5bbf8
Garrett Cullen <Garth619@users.noreply.github.com>
2021-10-05 19:48:46 -0700

    Added my project

--- wp-content/themes/mdk/functions.php
+++ wp-content/themes/mdk/functions.php
@@ -8,7 +8,7 @@ function load_my_styles_scripts()
 
     // Styles
 
-    wp_enqueue_style('styles', get_template_directory_uri() . '/style.css', '', 5, 'all');
+    // wp_enqueue_style('styles', get_template_directory_uri() . '/style.css', '', 5, 'all');
 
     // Disables jquery then registers it again to go into footer
 
@@ -25,7 +25,7 @@ add_action('wp_enqueue_scripts', 'load_my_styles_scripts', 20);
 /* CSS in Header for Lighthouse
 -------------------------------------------------------------- */
 
-//add_action('wp_head', 'merge_include_css');
+add_action('wp_head', 'merge_include_css');
 function merge_include_css()
 {
     $theme = wp_get_theme();
@@ -340,4 +340,18 @@ function cms_is_in_menu($menu = null, $object_id = null)
 
     return in_array((int) $object_id, $menu_items);
 
+}
+
+/* Add default image setting to ACF image fields
+-------------------------------------------------------------- */
+
+add_action('acf/render_field_settings/type=image', 'add_default_value_to_image_field');
+function add_default_value_to_image_field($field)
+{
+    acf_render_field_setting($field, array(
+        'label' => 'Default Image',
+        'instructions' => 'Appears when creating a new post',
+        'type' => 'image',
+        'name' => 'default_value',
+    ));
 }
